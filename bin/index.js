f938c63d48acb49ae52541ad3cc1b4bd8beaff9c
Owen Buckley <owenbuckley13@gmail.com>
2019-11-30 15:40:15 -0500

    issue-17 set node env for all scripts

--- bin/index.js
+++ bin/index.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-
 const app = require('../src/server/server');
 
 app.listen(3000);
--- src/server/server.js
+++ src/server/server.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 const express = require('express');
 const path = require('path');
 const git = require('simple-git')();
