f9c2b1d777d54aa25073749d8a2da31b243eeef6
Owen Buckley <owenbuckley13@gmail.com>
2019-11-29 19:24:25 -0500

    global install detection

--- src/server/server.js
+++ src/server/server.js
@@ -2,9 +2,15 @@ const express = require('express');
 const path = require('path');
 const git = require('simple-git')();
 const app = express();
+// https://stackoverflow.com/questions/5926672/where-does-npm-install-packages
+const nodeRootLocation = process.NODE_ENV === 'production'
+  ? process.platform === 'win32' 
+    ? path.join(process.execPath, '..')
+    : path.join(process.execPath, '..', '..')
+  : process.cwd();
 
 app.use(express.static(path.join(__dirname, '..', 'client')));
-app.use(express.static(path.join(process.cwd(), 'node_modules')));
+app.use(express.static(path.join(nodeRootLocation, 'node_modules')));
 
 app.get('/api/branch', (req, res) => {
   git.branchLocal((err, data) => {
