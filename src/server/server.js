dae6b5778b5721fc885f1c29fd4dd16710660179
Owen Buckley <owenbuckley13@gmail.com>
2019-12-02 20:59:31 -0500

    project favicon

--- src/server/server.js
+++ src/server/server.js
@@ -1,10 +1,12 @@
 #!/usr/bin/env node
 const express = require('express');
+const favicon = require('express-favicon');
 const path = require('path');
 const git = require('simple-git')();
 const app = express();
 
 app.use(express.static(path.join(__dirname, '..', 'client')));
+app.use(favicon(path.join(__dirname, '..', 'client', 'favicon.ico')));
 
 app.get('/api/branch', (req, res) => {
   git.branchLocal((err, data) => {
