51243548ccaec0dd53b2a425af8e89f3c4aaea95
Owen Buckley <owenbuckley13@gmail.com>
2019-12-02 20:45:55 -0500

    add output when starting server

--- bin/index.js
+++ bin/index.js
@@ -1,4 +1,8 @@
 #!/usr/bin/env node
 const app = require('../src/server/server');
 
-app.listen(3000);
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Git Explorer now running at localhost:${PORT}`);
+});
