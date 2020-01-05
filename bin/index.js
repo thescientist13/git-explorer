0b17466b84d0e8a5fdcec805794b704a27c5fc12
sowmiyamuthuraman <32141844+sowmiyamuthuraman@users.noreply.github.com>
2020-01-05 20:14:33 +0530

    Add functionality to automatically open the default browser on start … (#26)
    * Add functionality to automatically open the default browser on start of server.
    
    * Add open flag and update readme

--- bin/index.js
+++ bin/index.js
@@ -1,9 +1,16 @@
 #!/usr/bin/env node
 const app = require('../src/server/server');
 const argv = require('yargs').argv;
+const open = require('open');
 
 const PORT = argv.port || 3000;
+const PATH = `http://localhost:${PORT}`;
+const openBrowser = argv.open;
 
 app.listen(PORT, () => {
   console.log(`Git Explorer now running at localhost:${PORT}`);
+  
+  if (openBrowser) {
+    open(PATH);
+  }
 });
